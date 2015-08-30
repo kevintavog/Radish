a18041e8146e29b8d4d0235588b1925272d4fd63
kevintavog <kevin.tavog@zoho.com>
2015-08-30 11:48:32 -0700

    Start...

---
+++ src/Radish/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Radish
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate
+{
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var singleViewWindowController: SingleViewWindowController!
+
+    func applicationDidFinishLaunching(aNotification: NSNotification)
+    {
+        singleViewWindowController.initialize()
+    }
+}
---
+++ src/Radish/SingleViewWindowController.swift
@@ -0,0 +1,252 @@
+//
+//  SingleViewWindowController.swift
+//  Radish
+//
+
+import AppKit
+import AVFoundation
+import AVKit
+import Foundation
+
+
+class SingleViewWindowController: NSWindowController
+{
+    @IBOutlet weak var videoPlayer: AVPlayerView!
+    @IBOutlet weak var imageViewer: NSImageView!
+    @IBOutlet weak var statusTimestamp: NSTextField!
+    @IBOutlet weak var statusKeywords: NSTextField!
+    @IBOutlet weak var statusIndex: NSTextField!
+    @IBOutlet weak var statusLocation: NSTextField!
+    @IBOutlet weak var statusFilename: NSTextField!
+
+
+    var imageTypes = [String]()
+    let videoTypes = [AVFileTypeAIFC, AVFileTypeAIFF, AVFileTypeCoreAudioFormat, AVFileTypeAppleM4V, AVFileTypeMPEG4,
+        AVFileTypeAppleM4A, AVFileTypeQuickTimeMovie, AVFileTypeWAVE, AVFileTypeAMR, AVFileTypeAC3, AVFileTypeMPEGLayer3, AVFileTypeSunAU]
+
+    var supportedTypes = [String]()
+    var allFiles = [NSURL]()
+    var currentFileIndex = 0
+
+
+    func initialize()
+    {
+        window?.backgroundColor = NSColor.darkGrayColor()
+
+        videoPlayer.hidden = true
+        imageViewer.hidden = true
+
+        updateStatusView()
+
+        let cgImageTypes: NSArray = CGImageSourceCopyTypeIdentifiers()
+        imageTypes = cgImageTypes as [AnyObject] as! [String]
+
+        supportedTypes.appendContentsOf(imageTypes)
+        supportedTypes.appendContentsOf(videoTypes)
+
+    }
+
+    @IBAction func openFile(sender: AnyObject)
+    {
+        let dialog = NSOpenPanel()
+
+        dialog.allowedFileTypes = supportedTypes
+        dialog.canChooseDirectories = true
+        dialog.allowsMultipleSelection = false
+        if 1 != dialog.runModal() || dialog.URLs.count < 1
+        {
+            return
+        }
+
+
+        let localFile = dialog.URLs[0]
+        var isFolder:ObjCBool = false
+        let fileExists = NSFileManager.defaultManager().fileExistsAtPath(localFile.path!, isDirectory:&isFolder)
+        if !fileExists
+        {
+            return
+        }
+
+        var localFolder = localFile.path
+        if !isFolder
+        {
+            localFolder = localFile.URLByDeletingLastPathComponent?.path
+        }
+
+        let filesInFolder = (try! NSFileManager.defaultManager().contentsOfDirectoryAtPath(localFolder!)) as NSArray
+        allFiles = []
+        currentFileIndex = 0;
+
+        var index = 0
+        for f in filesInFolder
+        {
+            var fullName = localFolder! as NSString
+            fullName = fullName.stringByAppendingPathComponent(f as! String)
+
+            let fullUrl = NSURL(fileURLWithPath: fullName as String)
+            if (isSupportedFile(fullUrl))
+            {
+                allFiles.append(fullUrl)
+
+                if fullName == localFile.path
+                {
+                    currentFileIndex = allFiles.count - 1
+                }
+            }
+            else
+            {
+                print("Ignoring file \(fullName)")
+            }
+
+            ++index
+        }
+        
+        if (allFiles.count > 0)
+        {
+            displayFile(allFiles[currentFileIndex])
+        }
+    }
+
+    @IBAction func nextFile(sender: AnyObject)
+    {
+        displayFileByIndex(currentFileIndex + 1)
+    }
+
+    @IBAction func previousFile(sender: AnyObject)
+    {
+        displayFileByIndex(currentFileIndex - 1)
+    }
+
+    func displayFileByIndex(index: Int)
+    {
+        if (allFiles.count > 0)
+        {
+            currentFileIndex = index
+            if (currentFileIndex < 0) { currentFileIndex = allFiles.count - 1; }
+            if (currentFileIndex >= allFiles.count) { currentFileIndex = 0; }
+
+            displayFile(allFiles[currentFileIndex])
+        }
+    }
+
+    func displayFile(localFile: NSURL)
+    {
+        updateStatusView()
+
+        let fileType = getFileType(localFile.path!)
+        if imageTypes.contains(fileType)
+        {
+            displayImage(localFile)
+        }
+        else
+            if videoTypes.contains(fileType)
+            {
+                displayVideo(localFile)
+            }
+            else
+            {
+                displayUnsupportedFileType(localFile, fileType:fileType)
+            }
+    }
+
+    func displayImage(fileUrl: NSURL)
+    {
+        videoPlayer.player?.pause()
+
+        imageViewer.image = nil
+        if (imageViewer.hidden)
+        {
+            imageViewer.hidden = false
+            videoPlayer.hidden = true
+        }
+
+        Async.background
+        {
+            self.log("Loading \(fileUrl.lastPathComponent!)")
+            let imageSource = CGImageSourceCreateWithURL(fileUrl, nil)
+            let image = CGImageSourceCreateImageAtIndex(imageSource!, 0, nil)
+            let nsImage = NSImage(CGImage: image!, size: NSSize(width: CGImageGetWidth(image), height: CGImageGetHeight(image)))
+
+            Async.main
+            {
+                self.log("Setting the image...")
+                self.imageViewer.image = nsImage;
+                self.log("...Done")
+            }
+        }
+    }
+
+    func displayVideo(fileUrl: NSURL)
+    {
+        videoPlayer.player = AVPlayer(URL: fileUrl)
+        if (videoPlayer.hidden)
+        {
+            videoPlayer.hidden = false
+            imageViewer.hidden = true
+            imageViewer.image = nil
+        }
+        videoPlayer.player?.play()
+    }
+
+    func displayUnsupportedFileType(fileUrl: NSURL, fileType: String)
+    {
+        videoPlayer.player?.pause()
+        videoPlayer.hidden = true
+        imageViewer.hidden = true
+
+        print("Unhandled file type \(fileType) for '\(fileUrl)'")
+    }
+
+    func isSupportedFile(fullUrl:NSURL) -> Bool
+    {
+        var itemUti:String?
+        var uti:AnyObject?
+        do
+        {
+            try fullUrl.getResourceValue(&uti, forKey:NSURLTypeIdentifierKey)
+            itemUti = uti as? String
+        }
+        catch
+        {
+        }
+
+        return supportedTypes.contains(itemUti!)
+    }
+
+    func getFileType(filename:String) -> String
+    {
+        do {
+            return try NSWorkspace.sharedWorkspace().typeOfFile(filename)
+        } catch  {
+            return "";
+        }
+    }
+
+    func updateStatusView()
+    {
+        if (currentFileIndex < 0 || currentFileIndex >= allFiles.count || allFiles.count == 0)
+        {
+            statusFilename.stringValue = ""
+            statusTimestamp.stringValue = ""
+            statusLocation.stringValue = ""
+            statusKeywords.stringValue = ""
+            statusIndex.stringValue = ""
+        }
+        else
+        {
+            let fileUrl = allFiles[currentFileIndex]
+
+            statusIndex.stringValue = "\(currentFileIndex + 1) of \(allFiles.count)"
+            statusFilename.stringValue = "\(fileUrl.lastPathComponent!)"
+        }
+    }
+
+    func log(message:String)
+    {
+        NSLog(message)
+//        let dateFormatter = NSDateFormatter()
+//        dateFormatter.dateFormat = "HH:mm:ss A"
+//        let now = dateFormatter.stringFromDate(NSDate())
+//        print("\(now) - \(message)")
+    }
+}
